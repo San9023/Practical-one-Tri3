a9800f14b7e4e83d3ddf6d790188b0189fad910a
San9023 <118138970+San9023@users.noreply.github.com>
2022-11-25 10:02:28 +1100

    updated-prac-one

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function changeText(){
-    var textsArray = ["Text 1","Text 2", "Text 3", "Text 4", "Text 5"]
+    var textsArray = ["Unit1","Unit2", "Unit3", "Unit4", "Unit5"]
     var number = getRandomNumberBetween(0,textsArray.length - 1)
     console.log("Index: ", number)
     document.getElementById("heading").innerHTML = textsArray[number];
